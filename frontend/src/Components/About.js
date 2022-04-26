aa8a242b519f1881a993b5432f75a43051972fda
pratik nakave <nakave.pr@northeastern.edu>
2022-04-26 13:46:11 -0400

    About us page

---
+++ frontend/src/Components/About.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+export class About extends Component {
+  render() {
+    return (
+        <div>
+            OneStopWander
+          
+        </div>
+    )
+  }
+}
+
+export default About
